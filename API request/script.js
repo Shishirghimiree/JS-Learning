0482b35d776954ed88cd56e20a89a9ab90316f18
shishirghimiree <shishirghimirecsit077@indrenicollege.edu.np>
2024-12-27 22:37:27 +0545

    JS Learning

---
+++ API request/script.js
@@ -0,0 +1,15 @@
+const requestUrl ='https://api.github.com/users/shishirghimiree'
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET',requestUrl)
+xhr.onreadystatechange = function(){
+    console.log(xhr.readyState);
+    if(xhr.readyState===4){
+        const data =JSON.parse(this.responseText);
+        // const data =(this.responseText);
+
+        console.log(data.avatar_url);
+        console.log(data.name);
+    }
+}
+xhr.send();
